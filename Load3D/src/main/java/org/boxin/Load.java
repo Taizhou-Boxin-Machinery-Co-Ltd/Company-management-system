7ddc6a1947cbe6acf02706da63d32782cf1be31d
baka4n <qq474899581@gmail.com>
2022-10-31 20:08:30 +0800

    修改解析数据

--- Load3D/src/main/java/org/boxin/Load.java
+++ Load3D/src/main/java/org/boxin/Load.java
@@ -3,26 +3,40 @@ package org.boxin;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Load {
 	private int stpVersion;
+	protected String docStandards;
 	private final List<String> strings = new ArrayList<>();
-	private boolean isStep = false;
-	private boolean isIgs = false;
+	protected boolean isStep = false;
+	protected boolean isIgs = false;
+	protected final File file;
 
 
 	public Load(File file) {
+		this.file = file;
 		ap(file);
-		for (var a : strings) {
-			if (a.contains("STEP AP")) {
-				this.setStep(true);
-				String ap = a.substring(a.indexOf("AP") + 2);
-				var b = ap.substring(0,  ap.indexOf("'"));
-				stpVersion = Integer.parseInt(b);
-				System.out.println(stpVersion);
-				break;
+	}
+	private int i = 0;
+	public Load load3DProperties() {
+		for (var str : strings) {
+			if (str.contains("ISO-10303-21")) {
+				docStandards = str.replace(";", "").trim();
+			} else if (str.contains("HEADER")) {
+				i = 1;
+			}
+			if (i == 1 /*代表HEADER开始*/&& str.contains("FILE_DESCRIPTION")) {
+				//文件描述
+				String tmp = str.substring(str.indexOf("'") + 1, str.lastIndexOf("'"));
+				stpVersion = Integer.parseInt(tmp.substring(tmp.indexOf("AP") + 2).trim());
+				if (tmp.contains("AP")) {
+					this.setStep(true);
+				}
+
 			}
 		}
+		return this;
 	}
 
 	public int getStpVersion() {
@@ -47,7 +61,35 @@ public class Load {
 		return isStep;
 	}
 
-	private void setStep(boolean step) {
+	public void setStep(boolean step) {
 		isStep = step;
 	}
+
+	public static final List<Load> loads = new ArrayList<>();
+
+	public static List<File> getFiles(File file) {
+		List<File> files = new ArrayList<>();
+		for (var f : Objects.requireNonNull(file.listFiles())) {
+			if (f.isDirectory()) {
+				files.addAll(getFiles(f));
+			} else {
+				files.add(f);
+			}
+		}
+		return files;
+	}
+
+	public static void loadStep(File file, String[] subFix_) {
+		List<File> files = null;
+		if (file.exists() && file.isDirectory()) {
+			files = getFiles(file);
+		}
+		if (files != null) {
+			for (var f : files) {
+				final var subFix = f.getName().toLowerCase().substring(f.getName().toLowerCase().lastIndexOf(".") + 1);
+				for (var a : subFix_) if (subFix.equals(a)) loads.add(new Load(f).load3DProperties());
+			}
+		}
+	}
+
 }
--- Load3D/src/main/java/org/boxin/Main.java
+++ Load3D/src/main/java/org/boxin/Main.java
@@ -1,41 +1,15 @@
 package org.boxin;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Load> steps = new ArrayList<>();
 		File file = new File(System.getProperty("user.dir"), "解析三维");
-		List<File> files = null;
-		if (file.exists() && file.isDirectory()) {
-			files = getFiles(file);
-		}
-		if (files != null) {
-			for (var f : files) {
-				Load loadStep = new Load(f);
-				steps.add(loadStep);
-			}
+		Load.loadStep(file, new String[]{"step","stp"});
+		for (Load load : Load.loads) {
+			System.out.println(load.getStpVersion());
+
 		}
 	}
-	public static List<File> getFiles(File file) {
-		List<File> files = new ArrayList<>();
-		for (var f : Objects.requireNonNull(file.listFiles())) {
-			if (f.isDirectory()) {
-				files.addAll(getFiles(f));
-			} else {
-				files.add(f);
-			}
-		}
-		return files;
-	}
+
 }
