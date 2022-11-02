5754c4654d101965ac52d7002eace8d661745c82
baka4n <qq474899581@gmail.com>
2022-11-02 14:31:29 +0800

    update

--- Load3D/src/main/java/org/boxin/Main.java
+++ Load3D/src/main/java/org/boxin/Main.java
@@ -1,6 +1,7 @@
 package org.boxin;
 
 import org.boxin.d3d.FileLoad;
+import org.boxin.d3d.StpModule;
 
 import java.io.File;
 
@@ -9,8 +10,8 @@ public class Main {
 		File file = new File(System.getProperty("user.dir"), "解析三维");
 		FileLoad.getFile(file);
 		FileLoad.find();
-		for (var a : FileLoad.solidworksFiles) {
-			System.out.println(a);
+		for (var a : FileLoad.stp) {
+			StpModule stpModule = new StpModule(a);
 		}
 	}
 
--- Load3D/src/main/java/org/boxin/d3d/StpModule.java
+++ Load3D/src/main/java/org/boxin/d3d/StpModule.java
@@ -1,6 +1,8 @@
 package org.boxin.d3d;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author baka4n
@@ -8,6 +10,55 @@ import java.io.File;
  */
 @SuppressWarnings("JavadocDeclaration")
 public class StpModule {
-	public static void main(String[] args) {
+	private String coding_rules;
+	private final List<String> headerLine = new ArrayList<>();
+	private final List<String> dataLine = new ArrayList<>();
+	public StpModule(File loadFile) {
+		if (!loadFile.isDirectory()) {
+			try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(loadFile), "GBK"))) {
+				String line;
+				int node = 0;
+				while ((line = br.readLine()) != null) {
+					if (node == 1) {
+						if (line.contains("ENDSEC;")) {
+							node = 0;
+							continue;
+						}
+						headerLine.add(line);
+						continue;
+					}
+					if (node == 2) {
+						if (line.contains("ENDSEC;")) {
+							node = 0;
+							continue;
+						}
+						dataLine.add(line);
+						continue;
+					}
+					if (line.contains("ISO")) {
+						coding_rules = line;
+					}
+					if (line.contains("HEADER;")) {
+						node = 1;//头部
+					}
+					if (line.contains("DATA;")) {
+						node = 2;//数据区域
+					}
+				}
+				for (var a : headerLine) {
+					System.out.println(a);
+				}
+				for (var a : dataLine) {
+					System.out.println(a);
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class Header {
+
 	}
 }
