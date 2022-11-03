14b649179bc2ea8bfb061aa3077762dc8628b269
baka4n <qq474899581@gmail.com>
2022-11-03 22:18:53 +0800

    Update A.java

--- Load3D/src/main/java/org/boxin/basic/sec/sec/A.java
+++ Load3D/src/main/java/org/boxin/basic/sec/sec/A.java
@@ -10,4 +10,8 @@ public class A {
 	public static Entity advanced_face(String label, Entity[] set_face_bound, boolean BOOLEAN) {
 		return new Entity("advanced_face", label, set_face_bound, BOOLEAN);
 	}
+
+	public static Entity a3m_equivalence_accuracy_association(String id, String name, String description, Entity specific_accuracy, Entity accuracy) {
+		return new Entity("a3m_equivalence_accuracy_association", id, name, description, specific_accuracy, accuracy);
+	}
 }
