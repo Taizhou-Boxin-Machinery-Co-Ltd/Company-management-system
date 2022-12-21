9d400bc552a89d687a423bf702b623f66e05d8b6
baka4n <qq474899581@gmail.com>
2022-12-21 15:26:05 +0800

    添加自定义移位

---
+++ bxLocker/src/main/java/org/boxin/T.java
@@ -0,0 +1,35 @@
+package org.boxin;
+
+import org.boxin.utils.Hash;
+import org.boxin.utils.algorithm.Move;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class T {
+    public static void main(String[] args) throws Exception {
+        String a = "台州市博新机械有限公司";
+//        String hash = Hash.getSHA1(a);
+//        System.out.println(hash);
+        Hash hash = new Hash(a);
+        Move m = new Move(a.getBytes(StandardCharsets.UTF_8), 3);
+        final List<String> s = new ArrayList<>(m.getS());
+        m.lg();
+        final List<String> s1 = new ArrayList<>(m.getS());
+        m.rg();
+        final List<String> s2 = new ArrayList<>(m.getS());
+        for (int i = 0; i < s1.size(); i++) {
+            System.out.println(s.get(i) + ":" + s1.get(i) + ":" + s2.get(i));
+        }
+
+        System.out.println(hash.getSHA1());
+
+        System.out.println(hash.getSHA224());
+        System.out.println(hash.getSHA256());
+        System.out.println(hash.getSHA384());
+        System.out.println(hash.getSHA512());
+
+    }
+}
---
+++ bxLocker/src/main/java/org/boxin/utils/algorithm/Move.java
@@ -0,0 +1,51 @@
+package org.boxin.utils.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//自制移位
+public class Move {
+    private final List<String> s = new ArrayList<>();
+
+    public List<String> getS() {
+        return s;
+    }
+
+    private final int n;
+    public Move(byte[] bytes, int n) {
+        this.n = n;
+        for (var b : bytes) {
+            String t = String.format("%08x",b);
+            s.add(t);
+        }
+
+    }
+
+    public Move lg() {
+        List<String> sa = new ArrayList<>(s);
+        s.clear();
+        for (var b : sa) {
+            int m = n % 8;
+            String substring = b.substring(0, b.length() - m);
+            String substring1 = b.substring(b.length() - m);
+            String v = substring1 + substring;
+            s.add(v);
+        }
+        return this;
+    }
+
+    public Move rg() {
+        List<String> sa = new ArrayList<>(s);
+        s.clear();
+        for (var b : sa) {
+            int m = n / 8;
+
+            String substring = b.substring(0, m+3);
+            String substring1 = b.substring(m+3);
+            String v = substring1 + substring;
+            s.add(v);
+        }
+        return this;
+    }
+
+}
